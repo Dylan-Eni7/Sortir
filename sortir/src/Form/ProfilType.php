7819244fb0a3e2b29d61825941ee651fcb47a53d
Benoitldc <benoit.leduc54@gmail.com>
2021-03-18 15:38:53 +0100

    login + registration

--- sortir/src/Form/ProfilType.php
+++ sortir/src/Form/ProfilType.php
@@ -3,95 +3,63 @@
 namespace App\Form;
 
 use App\Entity\Site;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
+
+
 
 class ProfilType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', \Symfony\Component\Form\Extension\Core\Type\TextType::class,
+            ->add('username', TextareaType::class,
                 [
                     'label' => 'Pseudo :',
                 ])
-            ->add('nom', \Symfony\Component\Form\Extension\Core\Type\TextType::class,
+            ->add('nom', TextareaType::class,
                 [
                     'label' => 'Nom :',
                 ])
-            ->add('prenom', \Symfony\Component\Form\Extension\Core\Type\TextType::class,
+            ->add('prenom', TextareaType::class,
                 [
                     'label' => 'Prénom :',
                 ])
-            ->add('mail', \Symfony\Component\Form\Extension\Core\Type\TextType::class,
+            ->add('mail', EmailType::class,
                 [
                     'label' => 'E-mail :',
                 ])
-            ->add('telephone', \Symfony\Component\Form\Extension\Core\Type\TextType::class,
+            ->add('telephone', TextareaType::class,
                 [
                     'label' => 'Téléphone :',
                 ])
             ->add('site', EntityType::class, [
-                'label' => 'Campus :',
-                // looks for choices from this entity
+                'label' => 'Site :',
                 'class' => Site::class,
-
-                // uses the User.username property as the visible option string
-                'choice_label' => 'nom',
-
-                // used to render a select box, check boxes or radios
-                // 'multiple' => true,
-                // 'expanded' => true,
+                'choice_label' => 'nom'
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'label' => 'Mot de passe :',
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank(
-                        [
-                            'message' => 'Merci d\'entrer un mot de passe',
-                        ]
-                    ),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit contenir au minimum {{ limit }} caractères!',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 50,
-                        'maxMessage' => 'Votre mot de passe doit contenir au maximum {{ limit }} caractères!',
-                    ]),
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe :',
+                    'attr' => [
+                        'maxlength' => 50
+                    ]
                 ],
-            ])
-            ->add(
-                'newPassword',
-                PasswordType::class,
-                [
-
-                    'invalid_message' => 'Les mots de passe ne correspondent pas.',
-                    'required' => false,
-                    'label' => 'Comfirmation : ',
-                    'mapped' => false,
-                    'constraints' => [
-                        new Length(
-                            [
-                                'min' => 8,
-                                'minMessage' => 'Votre mot de passe doit contenir au minimum {{ limit }} caractères!',
-                                // max length allowed by Symfony for security reasons
-                                'max' => 50,
-                                'maxMessage' => 'Votre mot de passe doit contenir au maximum {{ limit }} caractères!',
-                            ]
-                        ),
-                    ],
+                'second_options' => [
+                    'label' => 'Confirmation :',
+                    'attr' => [
+                        'maxlength' => 50
+                    ]
                 ]
-            )
+            ])
             ->add('Enregistrer', SubmitType::class);
     }
 
