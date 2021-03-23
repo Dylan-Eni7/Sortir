a4adb2f83e8f3697a9bccaec66156ea25f3fd58b
jvadrot2020 <evermoredu57430@gmail.com>
2021-03-23 10:14:47 +0100

    ajout Commentaires AdminController

--- sortir/src/Controller/AdminController.php
+++ sortir/src/Controller/AdminController.php
@@ -34,14 +34,20 @@ class AdminController extends AbstractController
         EntityManagerInterface $entityManager,
         Request $request): Response
     {
+        //Si je ne suis pas admin je ne peux pas accéder a la page.
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        //Je créer le repository afin de récupérer un tableau contenant tout les sites existant.
         $siteRepository = $entityManager->getRepository(Site::class);
         $sites = $siteRepository->findAll();
 
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $site = new Site();
+
+        //Je créer un formulaire.
         $siteForm = $this->createForm(SiteType::class, $site);
         $siteForm->handleRequest($request);
 
+        //Si le formaulaire est valide, j'ajoute le nouveau Site en BDD.
         if ($siteForm->isSubmitted() && $siteForm->isValid()) {
 
             $entityManager->persist($site);
@@ -64,13 +70,15 @@ class AdminController extends AbstractController
      */
     public function modifySite(EntityManagerInterface $entityManager, Request $request): Response
     {
+        //Je récupère l'id et le nom du site qui sont en paramètres dans l'URL
         $id = $request->query->get("id");
-        $site = $entityManager->find(Site::class, $id);
-
         $nomSite = $request->query->get("nomSite");
 
-        $site->setNom(strtoupper($nomSite));
+        //Je recherche le Site qui correspond à l'id donné
+        $site = $entityManager->find(Site::class, $id);
 
+        //Je remplace le nom du Site actuel par le nom récupéré puis je sauvegarde le changement en BDD
+        $site->setNom(strtoupper($nomSite));
         $entityManager->flush();
 
         $this->addFlash('success', 'Votre site a bien été mis a jour !');
@@ -85,7 +93,11 @@ class AdminController extends AbstractController
     public function deleteSite($id, EntityManagerInterface $entityManager): Response
     {
         $siteRepository = $entityManager->getRepository(Site::class);
+
+        //Je récupère le Site en BDD selon l'id
         $site = $siteRepository->find($id);
+
+        //Je supprime le Site en BDD et sauvegarde le changement
         $entityManager->remove($site);
         $entityManager->flush();
 
@@ -100,16 +112,22 @@ class AdminController extends AbstractController
         EntityManagerInterface $entityManager,
         Request $request): Response
     {
+        //Si je ne suis pas admin, l'acces est refusé
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $villeRepository = $entityManager->getRepository(Ville::class);
+
+        //Je créer un tableau contenant toutes les villes de la BDD.
         $villes = $villeRepository->findAll();
 
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        //Je créer une nouvelle ville ainsi que le formulaire
         $ville = new Ville();
         $villeForm = $this->createForm(VilleType::class, $ville);
         $villeForm->handleRequest($request);
 
         if ($villeForm->isSubmitted() && $villeForm->isValid()) {
-
+            //Si le formulaire est valide
+            //Je met chaque première lettre de chaque mot en MAJ et l'assigne en nom de ville
             $ville->setNom(ucwords($villeForm->getData()->getNom(), " "));
             $entityManager->persist($ville);
             $entityManager->flush();
@@ -131,12 +149,17 @@ class AdminController extends AbstractController
      */
     public function modifyVille(EntityManagerInterface $entityManager, Request $request): Response
     {
+        //Je récupère l'id envoyé dans l'URL
         $id = $request->query->get("id");
+
+        //Je récupère la ville correspondante selon l'id
         $ville = $entityManager->find(Ville::class, $id);
 
+        //Je récupère le nom et le code postal envoyé dans l'URL
         $nomVille = $request->query->get("nomVille");
         $codePostal = $request->query->getInt("codePostal");
 
+        //J'assigne le nom et le code postal ç la ville que je modifie
         $ville->setNom(ucwords($nomVille, " "));
         $ville->setCodePostal($codePostal);
 
@@ -154,8 +177,11 @@ class AdminController extends AbstractController
     public function deleteVille($id, EntityManagerInterface $entityManager): Response
     {
         $villeRepository = $entityManager->getRepository(Ville::class);
+
+        //Je récupère la ville en BDD qui correspond a l'id envoyé
         $ville = $villeRepository->find($id);
 
+        //Je supprime la ville
         $entityManager->remove($ville);
         $entityManager->flush();
 
