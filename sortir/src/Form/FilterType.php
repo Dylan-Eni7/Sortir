0a39bee46774103ad4270896c86058973f04f826
Benoitldc <benoit.leduc54@gmail.com>
2021-03-25 10:07:48 +0100

    filter

--- sortir/src/Form/FilterType.php
+++ sortir/src/Form/FilterType.php
@@ -21,47 +21,40 @@ class FilterType extends AbstractType
                 'class' => Site::class,
                 'choice_label'  => 'nom',
                 'required'      => false,
-                'empty_data'    => null,
                 'mapped'        => false,
             ])
             ->add('dateHeureDebut',TextType::class,  [
                 'label'    => 'Date Evenement (début)',
                 'disabled'=>true,
                 'required'      => false,
-                'empty_data'    => null,
                 'mapped'        => false
             ])
             ->add('dateHeureFin' ,TextType::class,  [
                 'label'    => 'Date Evenement (fin)',
                 'disabled'=>true,
                 'required'      => false,
-                'empty_data'    => null,
                 'mapped'        => false
             ])
             ->add('organisateur', CheckboxType::class, [
                 'label'         => 'Sorties dont je suis organisateur',
                 'required'      => false,
-                'empty_data'    => null,
                 'mapped'        => false
             ])
             ->add('inscrit',CheckboxType::class,  [
                 'label'         => 'Sorties auxquelle je suis inscrit',
                 'required'      => false,
-                'empty_data'    => null,
                 'mapped'        => false,
                 'disabled'=>true,
             ])
             ->add('pasInscrit',CheckboxType::class,  [
                 'label'         => 'Sorties auxquelles je ne suis pas inscrit',
                 'required'      => false,
-                'empty_data'    => null,
                 'mapped'        => false,
         'disabled'=>true,
             ])
             ->add('passed',CheckboxType::class,  [
                 'label'         => 'Sorties passées',
                 'required'      => false,
-                'empty_data'    => null,
                 'mapped'        => false,
                 'disabled'=>true,
             ])
